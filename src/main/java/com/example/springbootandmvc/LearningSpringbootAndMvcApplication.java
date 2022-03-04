d7919b501bfe48fbea39d360ababe53b1d6b71b2
Miguel <miguel.perezsanchis@juliusbaer.com>
2022-03-04 12:07:39 +0100

    initial commit

---
+++ src/main/java/com/example/springbootandmvc/LearningSpringbootAndMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootandmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringbootAndMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringbootAndMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootandmvc/LearningSpringbootAndMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootandmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringbootAndMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
